9636767b71587305815782851c67dcc91a1a6ebd
Srujan Wankhede <srujannwankhede786@gmail.com>
2026-03-31 00:08:23 +0530

    new Project named  create..

---
+++ week-01-python/project/Variables_and_datatypes.py
@@ -0,0 +1,85 @@
+# a = 1
+
+# b = 2
+
+# c = 3
+
+# a, b, c = 4, 5, 6
+
+# a = c = 7
+
+# print(a, b, c)
+#####################
+
+# a = 3.14
+
+# answer = str(int(a))
+# print(type(answer))
+# print(answer)
+#####################
+
+# a = None
+# print(bool(a))
+# print(bool(str(a)))
+#####################
+
+# my_str = "Hello!"
+# op = my_str * -5
+# print("$", "", "$")
+#####################
+
+# age = float(input("How old are you?"))
+
+# months = (age-int(age))*12
+# days = (months - int(months)) * 30
+# print("Hello ! You are", int(age), "years,", int(months), "months and", int(days), "days old!")
+#####################
+
+# my_str = '12345678'
+# x = '1'
+
+# while x in my_str:
+#     print(x)
+#     print("add to", str(int(x) + 1))
+#     x += str(int(x) + 1)
+##########################
+# def cheer():
+#     print('I love Python programming!')
+
+# output = cheer()
+# print(output)
+###########################
+# def sum_of_cubes(start, end, step = 1):
+#     total = 0
+#     for i in range(start, end, step):
+#         total += i ** 3
+#     return total
+
+# result1 = sum_of_cubes(1, 4)
+# result2 = sum_of_cubes(2, 6, 2)
+
+# print(result1, result2)
+##########################
+# import math
+# from math import pi
+
+# result = round(math.sqrt(16) + pi)
+# print(result)
+############################
+# import random
+
+# random.seed(42)
+# result1 = random.randint(50, 100)
+# random.seed(42)
+# result2 = random.randint(50, 100)
+
+# print(result1, result2)
+##############################
+# gre = "Hello World!"
+# print(gre[1:-1])
+# print(gre[1:-1:2])
+
+###############################
+my_string = 'Hello, World!'
+print( i ) for i in range(0, len(my_string))
+print(my_string[7:12])
