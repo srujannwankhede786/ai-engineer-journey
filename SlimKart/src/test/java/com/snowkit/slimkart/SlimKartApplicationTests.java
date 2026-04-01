b01de698031977730d66e49ed8137c7958b682fc
Srujan Wankhede <srujannwankhede786@gmail.com>
2026-04-01 19:29:15 +0530

    unversioned intellij files

---
+++ SlimKart/src/test/java/com/snowkit/slimkart/SlimKartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snowkit.slimkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlimKartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
